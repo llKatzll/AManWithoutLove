f099791aba0e58540dfc49a45c1436619828744c
Ju Won <juwonpee@gmail.com>
2022-07-15 19:38:51 +0900

    asd
    asd

---
+++ Sauce.cpp
@@ -0,0 +1,17 @@
+#include "Sauce.h"
+#include <iostream>
+
+using namespace std;
+
+void test::input() {
+	int a;
+	cin >> a;
+
+	add += a;
+	number++;
+}
+
+int test::average() {
+	return add / number;
+}
+
---
+++ Sauce.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class test {
+public:
+	int add = 0;
+	int number = 0;
+	void input();
+	int average();
+
+};
+
+#pragma once
