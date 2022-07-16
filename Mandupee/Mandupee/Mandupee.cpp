3baf6804e18c45cf0d443a027b8c8cd58d08fcff
llKatzll <bluedia0113@gmail.com>
2022-07-16 19:30:42 +0900

    IWantFreeRamen
    IWantEat

---
+++ Mandupee/Mandupee/Mandupee.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <cstdint>
+#include <Windows.h>
+
+using namespace std;
+
+
+string engengeng() {
+    string eng = "engeng";
+    return eng;
+}
+
+int engengengeng(int a, int b) {
+    int a = 10;
+    int b = 4;
+    int c;
+    int c = a - b;
+
+    return c;
+}
+
+
+
+int main()
+{
+    int a;
+
+    cout << "hello" << endl;
+    
+    cin >> a;
+    cout << a << endl;
+
+    engengengeng(3, 18);
+}
+
+
+
