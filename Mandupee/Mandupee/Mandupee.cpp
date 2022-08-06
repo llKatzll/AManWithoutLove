ed13c8b909fe12a59ecaff0ddf6902a021922d46
llKatzll <bluedia0113@gmail.com>
2022-08-06 19:05:34 +0900

    NO
    sam.

--- Mandupee/Mandupee/Mandupee.cpp
+++ Mandupee/Mandupee/Mandupee.cpp
@@ -3,45 +3,34 @@
 
 using namespace std;
 
-int a;
-int b;
 
-int airhe() {
-	cin >> a;
-
-	return a;
-}
-
-int go() {
-	cin >> b;
-
-	return b * 2;
-}
-
-string poo() {
-	string a = "성";
-	string b = "이름";
-
-	cin >> a;
-	cin >> b;
-
-	return a + b;
-}
-
-void qoqo() {
-	//함수 3이랑 같지만 a 포인터로 값 넣기
-}
-
-bool roro() {
-	int a;
-
-	cin >> a;
-	if (
-}
 
 int main()
 {
+	int* a = new int;
+	a = 123;
+	
+	int* b = new int;
 
+	*b = 4;
+	
+	*b = *a + *b;
+
+	cout << *b << endl;
+
+	delete a;
+
+	*a = &b;
+
+	cout << *a << endl;
+
+	int c;
+
+	*c = &a;
+
+	delete a;
+
+	cout << c << endl;
 }
 
 
