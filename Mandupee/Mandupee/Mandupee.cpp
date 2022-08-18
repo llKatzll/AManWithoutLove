a495bf841e543fe89ed888a312cf2f7085780b6a
Ju Won <juwonpee@gmail.com>
2022-08-18 19:38:37 +0900

    asf

--- Mandupee/Mandupee/Mandupee.cpp
+++ Mandupee/Mandupee/Mandupee.cpp
@@ -5,32 +5,8 @@ using namespace std;
 
 
 
-int main()
-{
-	int* a = new int;
-	a = 123;
-	
-	int* b = new int;
 
-	*b = 4;
-	
-	*b = *a + *b;
-
-	cout << *b << endl;
-
-	delete a;
-
-	*a = &b;
-
-	cout << *a << endl;
-
-	int c;
-
-	*c = &a;
-
-	delete a;
-
-	cout << c << endl;
+int main() {
+	string a = "ABCDEFG";
+	cout << a.size() << endl;
 }
-
-
--- Mandupee/Mandupee/mandoo.h
+++ Mandupee/Mandupee/mandoo.h
@@ -1,11 +1,1 @@
 #pragma once
-
-class arihe {
-	public:
-		int a;
-		int b();
-		
-	private:
-		int d;
-		void e();
-};
