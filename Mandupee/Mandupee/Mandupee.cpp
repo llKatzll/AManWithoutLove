66892f78aa4785244bf1987ff0cb7fe05df2d062
llKatzll <bluedia0113@gmail.com>
2022-11-05 00:34:14 +0900

    wo

--- Mandupee/Mandupee/Mandupee.cpp
+++ Mandupee/Mandupee/Mandupee.cpp
@@ -1,118 +1,211 @@
 ﻿#include <iostream>
 #include <memory>
-#include<Windows.h>
+#include <Windows.h>
 
-using namespace std;
+using namespace std;	
 
-string a;
-string b;
-string d;
-int c;
-
-struct book {
-	int num;
-	string name;
-};
-
-void inquiry() {
-	struct book x[100];
-
-	Sleep(1000);
-	cout << "(익숙한 얼굴의 사서는 컴퓨터를 들고 오더니 물었다." << endl;
-	Sleep(2000);
-	cout << "몇 번째 칸에 있는 책 이었더라.." << endl;
-	Sleep(2000);
-	cout << "(사서는 컴퓨터를 타닥이며 중얼거렸다.)" << endl;
-	Sleep(2000);
-
-	cin >> c;
-
-	if (x[c].name != "") {
-		cout << "'책 있음'" << endl;
-		Sleep(1000);
+int q10(int factorial) {
+    int done = 1;
+    for (int i = 2; i <= factorial; i++){
+        done *= i;
 	}
-	else {
-		cout << "'책 없음'" << endl;
-	}
-	Sleep(1500);
+    return done;
 }
 
-void Registration() {
-	struct book x[100]; //순서 책
 
-	Sleep(1000);
-	cout << "오.. 알았어.." << endl;
-	Sleep(1000);
-	cout << "무슨 책 등..록하러 왔는지. 알려줄 수 있..어?" << endl;
-	Sleep(3000);
+void q11(int& c, int& d) {
+    int swwap;
+    swwap = c;
+    c = d;
+    d = swwap;
+}
 
-	cin >> b;
 
-	Sleep(2000);
-	cout << b + " 라는 책이라.. 이걸 등록해달라고?" << endl;
-	Sleep(2000);
-	cout << "오.. 알았오..." << endl;
+//12. 값 스왑
+void q12(int* c, int* d) {
+    int swap;
+    swap = *c;
+    *c = *d;
+    *d = swap;
+}
 
-	Sleep(2000);
-	cout << "알다싶이 여기 도서관은 매우 넓은 공간이라 책을 수납할 수 있는 장소가 가득해" << endl;
-	Sleep(3000);
-	cout << "(속삭이며) 사실 신설된 도서관이라 아직 책이 한 개도 없어" << endl;
-	Sleep(2000);
-	cout << "이 불쌍한 피주원 도서관에게 큼.  아까 " + b + " 라는 책을 등록하러 왔다고 했었지?" << endl;
-	Sleep(2000);
-	cout << "몇 번째 책장에 등록 해줄래?" << endl;
-	Sleep(4000);
-	cout << "아.. 나보고 하라고?" << endl;
-	Sleep(1000);
-	cout << "알았오... 몇 번째야?" << endl;
-	Sleep(2000);
+int q13(int arr[10]) {
 
-	cin >> c;
+    int note = 0;
+    //for 문 안에다가 0을 새겨버리면 반복할 때마다 0으로 초기화를 시켜버림
 
-	Sleep(2000);
-	cout << "(익숙하게 생긴 사람은 책을 받더니 금새 어디론가 달려갔다.)" << endl;
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] > note) {
+            note = arr[i];
+        }
+    }
+    return note;
+}
 
-	x[c].num = c;
-	x[c].name = b;
+void q14(int* arr) {
+    //c = b;
+    //b = a;
+    //a = c;
+    int c;
+    int b = 0;
+    int o = 9;
 
-	Sleep(3000);
-	cout << "(어디선가 책을 꽃고 오고 달려오는 사람이 눈에 띈다.)" << endl;
+    for (int a = 0; a < 6; a++) {
+
+        c = arr[b];
+        arr[b] = arr[o];
+        arr[o] = c;
+        b++;
+        o--;
+    }
+}
+
+int q15(int arr[8]) {
+    
+    int a = 0;
+    int b = 1;
+    int c = 2;
+    int d;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    for (int i = 0; i < 20; i++) {
+
+        d = a + b + c;
+        cout << d << endl;
+        a = b;
+        b = c;
+        c = d;
+    }
+    return c;
+    
+    
+}
+
+bool q16(int a) {
+    int b;
+
+    b = a % 13;
+
+    if (b == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+}
+
+void q17(string a) {
+
+}
+
+int q18(int a, int b) {
+    int note = 0;
+    int c;
+    for (int i = 0; i <= a; i++) {
+        c = i % b;
+        if (c == 0) {
+            note += i;
+        }
+        
+    }
+    return note;
 }
 
 int main() {
-	cout << "===================피주원 도서관========================" << endl << endl;
-	Sleep(2000);
-	cout << "피주원의 도서관에 오신걸 환영합니다" << endl;
-	Sleep(2000);
-	cout << "(익숙한 얼굴을 한 사람이 문을 열어준다)" << endl;
-	Sleep(4000);
-	cout << "(익숙한 사람의 얼굴을 한 사람의 뒤를 따라 걷고 있다.)" << endl;
-	Sleep(2000);
-	cout << "(침묵도 잠깐, 그 사람이 입을 열었다.)" << endl;
-	Sleep(2000);
-	cout << "ㅇ..여긴 뭐하러 왔어?" << endl;
-	Sleep(3000);
-	cout << ">> 등록" << endl;
-	cout << ">> 조회" << endl;
-	cout << endl;
-	cin >> a;
 
-	if (a == "등록") {
-		Registration();
-	}
-	if (a == "조회") {
-		Sleep(2000);
-		cout << "아직 ㅋㅋ 그 기능은 ㅋㅋ 지원을 안하는ㅋㅋ데" << endl;
-		Sleep(2000);
-		cout << "아.. 한다고?" << endl;
-		Sleep(1500);
-		cout << "미안해.." << endl;
-	}
-	else {
-		Sleep(2000);
-		cout << "엄.." << endl;
-		Sleep(1500);
-		cout << "도서관을 잘못 찾아온 것 같은데.." << endl;
-		Sleep(2000);
-	}
+    int p = 13;
+    int o = 3;
+    int l;
+
+    l = p % o;
+
+    if (l == 0) {
+        cout << "true" << endl;
+    }
+    else {
+        cout << "익사해라" << endl;
+    }
+
+
+	cout << "Q10: " << endl;
+	cout << q10(8) << endl;;
+	cout << endl;
+
+    {
+        int a = 10;
+        int b = 20;
+        cout << "Q11: " << endl;
+        q11(a, b);
+        cout << "A: " << a << endl;
+        cout << "B: " << b << endl;
+        cout << endl;
+    }
+
+    {
+        int a = 10;
+        int b = 20;
+        cout << "Q12: " << endl;
+        q12(&a, &b);
+        cout << "A: " << a << endl;
+        cout << "B: " << b << endl;
+        cout << endl;
+    }
+
+    {
+        cout << "Q13: " << endl;
+        int temp[] = { 3, 4, 8, 1, 342, 9, 10, 1, 3, 8 };
+        cout << q13(temp) << endl;
+        cout << endl;
+    }
+
+    {
+        cout << "Q14: " << endl;
+        int temp[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        q14(temp);
+        for (int x = 0; x < 10; x++) {
+            cout << temp[x] << endl;
+        }
+        cout << endl;
+    }
+
+    {
+        cout << "Q15: " << endl;
+        int temp[] = { 1, 0, 0, 0, 1, 1, 1, 0 };
+        cout << q15(temp) << endl;
+        cout << endl;
+    }
+
+    {
+        cout << "Q16: " << endl;
+        cout << q16(26) << endl;
+        cout << endl;
+    }
+
+    {
+        cout << "Q17: " << endl;
+        q17("BC");
+        cout << endl << endl;
+    }
+
+    {
+        cout << "Q18: " << endl;
+        cout << q18(15 ,3) << endl;
+        cout << endl;
+    }
 }
+
+
+
+/*
+10. 팩토리얼 계산기
+11. (보류)숫자 스왑하기. a와 b의 숫자를 스왑한다
+12. 숫자 스왑하기. c/d 포인터를 이용하여 a와 b의 숫자를 스왑한다
+13. 배열 안에 가장 높은 숫자 리턴하기
+14. 배열 스왑. 배열 안에 숫자들 순서를 거꾸로 만든다
+15. 배열 안에 2진수 숫자이다 eg: {1, 0, 0, 0, 1, 1, 1, 0}는 2진수로 10001110 2진수 숫자를 10진수로 리턴하라
+16. 핀 확인. int a가 입력 받은 핀 숫자이다. 핀이 13의 배수인지 확인하고 배수이면 true 리턴, 아니면 false 리턴
+ */
